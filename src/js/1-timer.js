c8f90f16465823d8295925f54bde0e1fa423d158
Andriy Kiselyov <168927164+andrelux-ua@users.noreply.github.com>
2025-01-11 00:44:05 +0200

    timer

---
+++ src/js/1-timer.js
@@ -0,0 +1,88 @@
+import flatpickr from 'flatpickr';
+import iziToast from 'izitoast';
+const startButton = document.querySelector('[data-start]');
+const datetimePicker = document.querySelector('#datetime-picker');
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
+
+let userSelectedDate = null;
+let timerId = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const now = new Date();
+    if (selectedDates[0] <= now) {
+      iziToast.error({
+        title: 'Invalid Date',
+        message: 'Please choose a date in the future',
+      });
+      startButton.disabled = true;
+    } else {
+      startButton.disabled = false;
+      userSelectedDate = selectedDates[0];
+    }
+  },
+};
+
+flatpickr(datetimePicker, options);
+
+startButton.addEventListener('click', () => {
+  startButton.disabled = true;
+  datetimePicker.disabled = true;
+  timerId = setInterval(() => {
+    const now = new Date();
+    const timeLeft = userSelectedDate - now;
+
+    if (timeLeft <= 0) {
+      clearInterval(timerId);
+      iziToast.success({
+        title: "Time's Up!",
+        message: 'The countdown has finished.',
+      });
+      resetTimer();
+      return;
+    }
+
+    updateTimer(convertMs(timeLeft));
+  }, 1000);
+});
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function updateTimer({ days, hours, minutes, seconds }) {
+  daysEl.textContent = addLeadingZero(days);
+  hoursEl.textContent = addLeadingZero(hours);
+  minutesEl.textContent = addLeadingZero(minutes);
+  secondsEl.textContent = addLeadingZero(seconds);
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function resetTimer() {
+  datetimePicker.disabled = false;
+  startButton.disabled = true;
+  daysEl.textContent = '00';
+  hoursEl.textContent = '00';
+  minutesEl.textContent = '00';
+  secondsEl.textContent = '00';
+}
